ba8f33d9fa2fc4c943e3631e763f6700188f1a89
whdwls248 <dmlrud1005@naver.com>
2024-06-24 17:23:08 +0900

    Login form 완성

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'chatting-app';
+
+  onAddPost(form: NgForm) {
+    console.log('email: ', form.value.email)
+    console.log('password: ', form.value.password)
+  }
 }
